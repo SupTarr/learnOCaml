4754e567d33fe7691a1c2a33daac7c147e8eb15c
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2022-12-24 11:56:03 +0700

    Finish options

---
+++ options.ml
@@ -0,0 +1,16 @@
+let get_val = function
+  | None -> failwith "??"
+  | Some x -> x
+
+let get_val default = function
+  | None -> default
+  | Some x -> x
+
+let rec list_max (lst : 'a list) : 'a option =
+  match lst with
+  | [] -> None
+  | h :: t -> begin
+    match list_max t with
+    | None -> Some h
+    | Some m -> Some (max h m)
+  end
